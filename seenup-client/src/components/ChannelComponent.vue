2bc986148fb56a78c9154f38f15f8376bd40363c
Tomáš Drga <tomasxdrga@gmail.com>
2024-12-09 12:42:07 +0100

    Channel component status indicator fixed

--- seenup-client/src/components/ChannelComponent.vue
+++ seenup-client/src/components/ChannelComponent.vue
@@ -1,12 +1,11 @@
 <template>
   <div v-if="channelsStore.active">
-    <div v-if="showInfiniteScroll">
       <q-infinite-scroll ref="infiniteScroll" @load="onLoad" reverse>
-        <template v-slot:loading>
+       <!--  <template v-slot:loading>
           <div class="row justify-center q-my-md">
             <q-spinner color="deep-purple-4" name="dots" size="40px" />
           </div>
-        </template>
+        </template> -->
 
         <!--  Generate the chat messages -->
 
@@ -28,7 +27,6 @@
         </span>
         </template>
       </q-infinite-scroll>
-    </div>
   </div>
   <div v-else>
     <div class="row justify-center items-center q-ma-md">
@@ -82,8 +80,13 @@ const getUserStatus = (userId: number) => {
 };
 
 const messages = computed(() => {
-  if (user.value?.status === 'offline') {
+  if (!user.value) {
+    return [];
+  }
+  const status = user.value ? getUserStatus(user.value.id) : 'offline';
+  if (status === 'offline') {
     console.log('User is offline, retaining last known messages.');
+    console.log('user status', status);
     return lastKnownMessages.value; // Return the last known messages
   }
 
@@ -191,12 +194,13 @@ watch(() => channelsStore.notifications, async () => {
   await nextTick();
   if (!permissionGranted.value || $q.appVisible) return;
   console.log('Checking notifications', channelsStore.notifications);
+  const status = user.value ? getUserStatus(user.value.id) : 'offline';
   for (const notification of channelsStore.notifications) {
     if (
       notification.content.startsWith('/') ||
       userNotificationSetting.value === 'Off' ||
-      user.value?.status === 'dnd' ||
-      user.value?.status === 'offline'
+      status === 'dnd' ||
+      status === 'offline'
     ) return;
     const cleanMessage = notification.content.replace(/<[^>]*>/g, '').trim();
     const processedMessage = cleanMessage.length > 25
